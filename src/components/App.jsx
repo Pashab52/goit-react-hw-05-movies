09e2659e7dd39dd90214eb95393abc8ebfcd90e1
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-08-02 00:11:38 +0300

    layout/mov.det 0.8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Home from 'pages/Home';
 import MovieDetails from 'pages/MovieDetails'
 import { Route, Routes } from 'react-router-dom'
 import { Header } from './Header/Header';
+import {Layout} from './Layout'
 
 
 
@@ -10,18 +11,23 @@ export const App = () => {
     <div
       style={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
       }}
     >
-      
       <Routes>
-        <Route path="/" element={<Header />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/movies" element={<div>Movies</div>} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
+          <Route path="movies" element={<div>Movies</div>} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<div>Cast</div>} />
+            
+            <Route path="reviews" element={<div>Reviews</div>} />
+            
+          </Route>
         </Route>
       </Routes>
     </div>
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,74 +1,110 @@
-import { useParams } from "react-router-dom";
-import { fetchMovieDetails } from "services/movieApi";
+import { Link, useParams, Outlet } from 'react-router-dom';
+import { fetchMovieDetails } from 'services/movieApi';
 
-import { useEffect, useState } from "react";
-
+import { useEffect, useState } from 'react';
 
 const MovieDetails = () => {
-    const [movieDetails, setMovieDetails] = useState(null);
-const { movieId } = useParams();
-    console.log(movieId);
-    
+  const [movieDetails, setMovieDetails] = useState(null);
+  const { movieId } = useParams();
 
-useEffect(() => {
+  // Поясніть, будь ласка, чому компонент рендериться по 3 - 4 рази (консольлог стільки разів спрацьовує)
+  console.log('render MovieDetails');
+
+  useEffect(() => {
+     console.log('render useEf MovieDetails');
+    if (movieDetails){return}
     const getMovieDetails = async () => {
+      const movieDetailsData = await fetchMovieDetails(movieId);
 
-        const movieDetailsData = await fetchMovieDetails(movieId);
+      const normMovieDetailsData = normalizeMovieDetailsData(movieDetailsData);
+      setMovieDetails(normMovieDetailsData);
+    };
 
-        const normMovieDetailsData =
-            normalizeMovieDetailsData(movieDetailsData);
-        setMovieDetails(normMovieDetailsData);
+    try {
+      getMovieDetails();
+    } catch (error) {
+      console.error(error);
     }
+  });
 
-        try {
-           getMovieDetails();
-        } catch (error) {
-            console.error(error)
-        }
-
-    
-},[movieId])
-    
-    
-    function normalizeMovieDetailsData({
-        poster_path,
-        original_title,
+  function normalizeMovieDetailsData({
+    poster_path,
+    title,
+    release_date,
+    vote_average,
+    overview,
+    genres,
+  }) {
+    return {
+      poster_path,
+      title,
       release_date,
       vote_average,
       overview,
       genres,
-    }) {
-        return {
-          poster_path,
-            original_title,
-          release_date,
-          vote_average,
-          overview,
-          genres,
-        }
-    }
+    };
+  }
 
-console.log(movieDetails);
-    return (
-      movieDetails && (
-        <>
-          <div>
-            <img src="" alt="" />
+  // const { poster_path,
+  //   original_title,
+  //   release_date,
+  //   vote_average,
+  //   overview,
+  //   genres=[]} = movieDetails;
+
+  // console.log(movieDetails);
+
+  const genresData = () => {
+    return movieDetails.genres.map(({ name }) => name).join(', ');
+  };
+
+  const normalizeDate = () => {
+    return Number.parseInt(movieDetails.release_date);
+  };
+
+  const normalizeScore = () => {
+    return Math.round(movieDetails.vote_average * 10);
+  }
+
+  return (
+    movieDetails && (
+      <>
+        <div className="details-wrapper">
+          <div className="details-img-wrapper">
+            <img
+              className="details-img"
+              src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`}
+              alt={movieDetails.title}
+              width="300px"
+            />
           </div>
           <div>
-            <h1>{movieDetails.original_title}</h1>
-            <p>{movieDetails.release_date}</p>
+            <h1>
+              {movieDetails.title} ({normalizeDate()})
+            </h1>
+            <p>User score: {normalizeScore()}%</p>
             <h2>Overview</h2>
             <p>{movieDetails.overview}</p>
             <h2>Genres</h2>
-            <p></p>
-          </div>
-        </>
-      )
-    );
-    
-    
-}
 
+            <span>{genresData()}</span>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link to="cast">Cast</Link>
+            </li>
+
+            <li>
+              <Link to="reviews">Reviews</Link>
+            </li>
+          </ul>
+        </div>
+        <Outlet/>
+      </>
+    )
+  );
+};
 
 export default MovieDetails;
