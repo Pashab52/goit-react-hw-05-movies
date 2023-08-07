26db24ea0f17a4de21860725a8627823595cf6bf
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-08-07 11:40:20 +0300

    fix title

--- src/components/TrendsItem/TrendsItem.jsx
+++ src/components/TrendsItem/TrendsItem.jsx
@@ -5,19 +5,23 @@ import {Link, useLocation} from 'react-router-dom'
 export const TrendsItem = ({ title, name, id, backdrop_path }) => {
   const location = useLocation();
      return (
-      <li>
-        <Link
-          to={`movies/${id}`}
-          state={{ from: location }}
-          className={css.trend}
-          style={{
-            backgroundImage: `url("https://image.tmdb.org/t/p/w500${backdrop_path}")`,
-          }}
-        >
-          {title ? <p>{title} </p> : <p>{name}</p>}
-        </Link>
-      </li>
-    );
+       <li>
+         <Link
+           to={`movies/${id}`}
+           state={{ from: location }}
+           className={css.trend}
+           style={{
+             backgroundImage: `url("https://image.tmdb.org/t/p/w500${backdrop_path}")`,
+           }}
+         >
+           {title ? (
+             <p className={css.title}> {title} </p>
+           ) : (
+             <p className={css.title}>{name}</p>
+           )}
+         </Link>
+       </li>
+     );
   
 }
 
